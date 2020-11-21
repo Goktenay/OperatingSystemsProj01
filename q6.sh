a7c198809a3bc838c76502b5976f79addbaf350c
alpago <alpagogoktenay@hotmail.com>
2020-11-22 00:09:42 +0300

    Working q5 (q6) added

--- q6.sh
+++ q6.sh
@@ -3,31 +3,70 @@ clear
 
 cpy() 
 {
-
-	directories=$(ls -d */ 2>/dev/null)
-	filesToCopy=$(ls -p | grep -v /)
-
-	if test ! -z "$filesToCopy"
+	## is there a copy folder already?
+	if test ! -d "./copyfolder"
 	then
-		mkdir copyfolder
-		while IFS= read -r line	
-		do
-		cp "$line" copyfolder
-		done <<<"$filesToCopy"
-	fi
+		fileDescriptor=$2 ## file description
+		isRecursive=$3 ## if its recursive then 1 else 0
+		
+		## test if it's recursive
+		if test "$isRecursive" -eq 1
+		then
+			directories=$(ls -d */ 2>/dev/null)
+		fi
+		
+		## Files to copy list
+		filesToCopy=$( (ls -p $fileDescriptor ) | grep -v /)
 
+	
+		## if there are no files to copy
+		if test ! -z "$filesToCopy"
+		then
+			mkdir copyfolder
+			while IFS= read -r line	
+			do
+			cp "$line" copyfolder ## copy to folder
+			done <<<"$filesToCopy"
+		fi
 
-	if test ! -z "$directories"
-	then
-		while IFS= read -r line
-		do
-		cd "$1/$line"
-		cpy "$(pwd)"
-		cd ..
-		done <<<"$directories"
+		
+		## if it's recursive
+		if test "$isRecursive" -eq 1
+		then
+			if test ! -z "$directories"
+			then
+				while IFS= read -r line
+				do
+				cd "$1/$line" ## change directory
+				cpy "$(pwd)" "$fileDescriptor" "$isRecursive" ## aply recursive call
+				cd .. ## pop back to previous directory
+				done <<<"$directories"
+			fi
+		fi
+	else
+		echo "We already copied the content"
 	fi
 	
 }
 
+firstArg=$1
+secondArg=$2
 
-cpy "$(pwd)"
+if test "$firstArg" = "-R" -o "$firstArg" = "-r" ## if our first arg is an option
+then
+	if test -z "$secondArg"
+	then
+		echo "file name descriptor is missing"
+	else
+		cpy "$(pwd)" "$secondArg" 1
+	fi
+else
+	if test -z "$firstArg" 
+	then
+		echo "incorrect number of arguments"
+	else
+		cpy "$(pwd)" "$firstArg" 0 
+	fi
+fi
+
+
